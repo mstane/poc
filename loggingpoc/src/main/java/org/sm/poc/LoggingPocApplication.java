fe3ca632bdf3e1f8c563bfb6290dedb20a5afa7b
Stanislav Markov <stan.markov@regnosys.com>
2020-01-02 14:57:03 +0000

    Kick off GC monitor

--- loggingpoc/src/main/java/org/sm/poc/LoggingPocApplication.java
+++ loggingpoc/src/main/java/org/sm/poc/LoggingPocApplication.java
@@ -5,6 +5,8 @@ import io.dropwizard.Application;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.github.gcmonitor.GcMonitor;
+import io.github.gcmonitor.integration.dropwizard.DropwizardAdapter;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.dropwizard.DropwizardExports;
 import org.sm.poc.core.MetricsSender;
@@ -13,6 +15,7 @@ import org.sm.poc.metrics.CounterExample;
 import org.sm.poc.resources.EchoResource;
 import org.sm.poc.resources.EnvResource;
 
+import java.time.Duration;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -51,6 +54,7 @@ public class LoggingPocApplication extends Application<LoggingPocConfiguration> 
 
 //        startMetricsSender();
 
+        // Custom metric example
         new CounterExample(environment.metrics());
 
 //        CollectorRegistry.defaultRegistry.register(new DropwizardExports(new MetricRegistry()));
@@ -61,11 +65,22 @@ public class LoggingPocApplication extends Application<LoggingPocConfiguration> 
                 .addServlet("prometheusMetrics", new io.prometheus.client.exporter.MetricsServlet(collectorRegistry))
                 .addMapping("/prometheusMetrics");
 
+        startGcMonitor(environment.metrics());
+
     }
 
     private void startMetricsSender() {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
         scheduledExecutorService.scheduleAtFixedRate(new MetricsSender(), 20,60, TimeUnit.SECONDS);
+    }
+
+    private void startGcMonitor(MetricRegistry registry) {
+        GcMonitor gcMonitor = GcMonitor.builder()
+                .addRollingWindow("15min", Duration.ofMinutes(15))
+                .build();
+        gcMonitor.start();
+
+        registry.registerAll(DropwizardAdapter.toMetricSet("jvm-gc-monitor", gcMonitor));
     }
 
 }
